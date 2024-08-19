6b6912f6d7eb8d7ae4d04f48c6f79dc5244746e0
Floakz <sergioamoreira@sapo.pt>
2024-08-19 13:22:19 +0100

    Update portfolio gif

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
-const eachCard = document.querySelectorAll('.step-card')
-const animationIncard = document.querySelectorAll('.animation-incard')
-
-
+const eachCard = document.querySelectorAll('.step-card');
 
 for (let card of eachCard) {
-    card.addEventListener('mouseover', () => {
-        let animation = card.querySelector('.animation-incard')
-        let info = card.querySelector('.whatwebring-p-hidden')
-        animation.classList.add('animation-incard-hidden')
-        info.classList.add('whatwebring-p')
-    })
+    card.addEventListener('mouseover', handleMouseOverOut);
+    card.addEventListener('mouseout', handleMouseOverOut);
+    card.addEventListener('click', handleClick);
 }
 
+function handleMouseOverOut(e) {
+    const animation = e.currentTarget.querySelector('.animation-incard');
+    const info = e.currentTarget.querySelector('.whatwebring-p-hidden');
 
-
-for (let card of eachCard) {
-    card.addEventListener('mouseout', () => {
-        let animation = card.querySelector('.animation-incard')
-        let info = card.querySelector('.whatwebring-p-hidden')
-        animation.classList.remove('animation-incard-hidden')
-        info.classList.remove('whatwebring-p')
-    })
+    if (e.type === 'mouseover') {
+        animation.classList.add('animation-incard-hidden');
+        info.classList.add('whatwebring-p');
+    } else if (e.type === 'mouseout') {
+        animation.classList.remove('animation-incard-hidden');
+        info.classList.remove('whatwebring-p');
+    }
 }
+
+function handleClick(e) {
+    const animation = e.currentTarget.querySelector('.animation-incard');
+    const info = e.currentTarget.querySelector('.whatwebring-p-hidden');
+    animation.classList.toggle('animation-incard-hidden');
+    info.classList.toggle('whatwebring-p');
+}
+
 
