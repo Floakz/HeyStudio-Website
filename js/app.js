488e558a4995ef53029d7777b315398b6bf3921e
Floakz <sergioamoreira@sapo.pt>
2024-08-20 10:32:39 +0100

    Fix responsive sizes
    Made adjustments to the design so that it becomes more responsive and visualy balanced on mobile

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 const eachCard = document.querySelectorAll('.step-card');
 
 for (let card of eachCard) {
-    card.addEventListener('mouseover', handleMouseOverOut);
-    card.addEventListener('mouseout', handleMouseOverOut);
+    // card.addEventListener('mouseover', handleMouseOverOut);
+    // card.addEventListener('mouseout', handleMouseOverOut);
     card.addEventListener('click', handleClick);
 }
 
-function handleMouseOverOut(e) {
-    const animation = e.currentTarget.querySelector('.animation-incard');
-    const info = e.currentTarget.querySelector('.whatwebring-p-hidden');
+// function handleMouseOverOut(e) {
+//     const animation = e.currentTarget.querySelector('.animation-incard');
+//     const info = e.currentTarget.querySelector('.whatwebring-p-hidden');
 
-    if (e.type === 'mouseover') {
-        animation.classList.add('animation-incard-hidden');
-        info.classList.add('whatwebring-p');
-    } else if (e.type === 'mouseout') {
-        animation.classList.remove('animation-incard-hidden');
-        info.classList.remove('whatwebring-p');
-    }
-}
+//     if (e.type === 'mouseover') {
+//         animation.classList.add('animation-incard-hidden');
+//         info.classList.add('whatwebring-p');
+//     } else if (e.type === 'mouseout') {
+//         animation.classList.remove('animation-incard-hidden');
+//         info.classList.remove('whatwebring-p');
+//     }
+// }
 
 function handleClick(e) {
     const animation = e.currentTarget.querySelector('.animation-incard');
